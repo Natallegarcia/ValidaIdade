d40d126fabc261dba86a71a760056e107cd305bc
Nátalle Garcia <102543825+Natallegarcia@users.noreply.github.com>
2023-07-06 21:46:12 -0300

    Adicionar arquivos de projeto.

---
+++ frmTelaPrincipal2.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmTelaPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(frmTelaPrincipal))
+        PictureBox1 = New PictureBox()
+        btnPrincipal = New Button()
+        lbl1 = New Label()
+        txtIdade = New TextBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        resources.ApplyResources(PictureBox1, "PictureBox1")
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.TabStop = False
+        ' 
+        ' btnPrincipal
+        ' 
+        resources.ApplyResources(btnPrincipal, "btnPrincipal")
+        btnPrincipal.BackColor = SystemColors.ButtonHighlight
+        btnPrincipal.Name = "btnPrincipal"
+        btnPrincipal.UseVisualStyleBackColor = False
+        ' 
+        ' lbl1
+        ' 
+        resources.ApplyResources(lbl1, "lbl1")
+        lbl1.Name = "lbl1"
+        ' 
+        ' txtIdade
+        ' 
+        resources.ApplyResources(txtIdade, "txtIdade")
+        txtIdade.BackColor = SystemColors.ScrollBar
+        txtIdade.Name = "txtIdade"
+        ' 
+        ' frmTelaPrincipal
+        ' 
+        resources.ApplyResources(Me, "$this")
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ButtonHighlight
+        CausesValidation = False
+        Controls.Add(PictureBox1)
+        Controls.Add(txtIdade)
+        Controls.Add(lbl1)
+        Controls.Add(btnPrincipal)
+        ForeColor = SystemColors.Highlight
+        Name = "frmTelaPrincipal"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnPrincipal As Button
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents txtIdade As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ frmTelaPrincipal2.vb
@@ -0,0 +1,46 @@
+﻿Public Class frmTelaPrincipal
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+
+
+    End Sub
+
+    Private Sub btnPrincipal_Click(sender As Object, e As EventArgs) Handles btnPrincipal.Click
+
+
+
+        Dim idadeUsuario As Integer
+        Dim idadeMinima As Integer
+        Dim idadeMaxima As Integer
+        idadeUsuario = txtIdade.Text
+        idadeMinima = 12
+        idadeMaxima = 60
+
+        MsgBox("A idade informada é " + idadeUsuario.ToString)
+
+
+
+
+
+        If idadeUsuario < 12 Then
+            MsgBox("O valor da idade é mínima para esse brinquedo é " + idadeMinima.ToString, MsgBoxStyle.Critical, "IDADE MÍNIMA")
+        ElseIf idadeUsuario <= 59 Then
+            MsgBox("Sua idade é a ideal para este brinquedo no parque ", MsgBoxStyle.MsgBoxRight, "Passagem Liberada")
+
+        ElseIf idadeUsuario > 59 Then
+            MsgBox("O valor máximo da idade para esse brinquedo é  " + idadeMaxima.ToString, MsgBoxStyle.Critical, "IDADE MÁXIMA")
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub frmTelaPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
