1d50c0548d49ff83aedf70e750c792b5dc9f4be7
Nátalle Garcia <102543825+Natallegarcia@users.noreply.github.com>
2023-07-12 18:51:39 -0300

    Second Commit

---
+++ 4 - Controle de Fluxo/frmPrincipal4.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmPrincipal4
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnPrincipal = New Button()
+        lblNomeProjeto = New Label()
+        txtIdade = New TextBox()
+        lblIdade = New Label()
+        txtResultado = New TextBox()
+        lblResultado = New Label()
+        SuspendLayout()
+        ' 
+        ' btnPrincipal
+        ' 
+        btnPrincipal.Location = New Point(12, 147)
+        btnPrincipal.Name = "btnPrincipal"
+        btnPrincipal.Size = New Size(125, 66)
+        btnPrincipal.TabIndex = 0
+        btnPrincipal.Text = "&Clique Aqui"
+        btnPrincipal.UseVisualStyleBackColor = True
+        ' 
+        ' lblNomeProjeto
+        ' 
+        lblNomeProjeto.AutoSize = True
+        lblNomeProjeto.Location = New Point(12, 15)
+        lblNomeProjeto.Name = "lblNomeProjeto"
+        lblNomeProjeto.Size = New Size(41, 15)
+        lblNomeProjeto.TabIndex = 1
+        lblNomeProjeto.Text = "Label1"
+        ' 
+        ' txtIdade
+        ' 
+        txtIdade.Location = New Point(12, 92)
+        txtIdade.Name = "txtIdade"
+        txtIdade.Size = New Size(100, 23)
+        txtIdade.TabIndex = 2
+        ' 
+        ' lblIdade
+        ' 
+        lblIdade.AutoSize = True
+        lblIdade.Location = New Point(10, 53)
+        lblIdade.Name = "lblIdade"
+        lblIdade.Size = New Size(41, 15)
+        lblIdade.TabIndex = 3
+        lblIdade.Text = "Label1"
+        ' 
+        ' txtResultado
+        ' 
+        txtResultado.BorderStyle = BorderStyle.FixedSingle
+        txtResultado.Location = New Point(10, 268)
+        txtResultado.Name = "txtResultado"
+        txtResultado.ReadOnly = True
+        txtResultado.Size = New Size(423, 23)
+        txtResultado.TabIndex = 4
+        ' 
+        ' lblResultado
+        ' 
+        lblResultado.AutoSize = True
+        lblResultado.Location = New Point(12, 235)
+        lblResultado.Name = "lblResultado"
+        lblResultado.Size = New Size(41, 15)
+        lblResultado.TabIndex = 5
+        lblResultado.Text = "Label1"
+        ' 
+        ' frmPrincipal4
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(857, 450)
+        Controls.Add(lblResultado)
+        Controls.Add(txtResultado)
+        Controls.Add(lblIdade)
+        Controls.Add(txtIdade)
+        Controls.Add(lblNomeProjeto)
+        Controls.Add(btnPrincipal)
+        Name = "frmPrincipal4"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Aplicação Hello World"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnPrincipal As Button
+    Friend WithEvents lblNomeProjeto As Label
+    Friend WithEvents txtIdade As TextBox
+    Friend WithEvents lblIdade As Label
+    Friend WithEvents txtResultado As TextBox
+    Friend WithEvents lblResultado As Label
+End Class
---
+++ 4 - Controle de Fluxo/frmPrincipal4.vb
@@ -0,0 +1,62 @@
+﻿Public Class frmPrincipal4
+
+
+    Public Sub New()
+
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        lblNomeProjeto.Text = "4- Controle de Fluxo"
+        btnPrincipal.Text = "Verificar permissão de entrada"
+        Me.Text = "4 Controle de Fluxo"
+
+        lblIdade.Text = "Idade"
+        lblResultado.Text = "Resultado da análise: "
+
+
+
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+
+    End Sub
+
+    Private Sub btnPrincipal_Click(sender As Object, e As EventArgs) Handles btnPrincipal.Click
+
+        Dim idadeUsuario As Integer
+        Dim permitido As String
+        Dim bloqueado As String
+        Dim resultado As String
+
+        idadeUsuario = txtIdade.Text
+        resultado = txtResultado.Text
+        permitido = "Você tem permissão para prosseguir"
+        bloqueado = "É necessário o acompanhamento dos responsáveis legais!"
+
+        If idadeUsuario <= 11 Then
+            resultado = bloqueado
+            If resultado = bloqueado Then
+                MsgBox("Está acompanhado dos seus responsáveis legais? ", MsgBoxStyle.YesNo, "Validação")
+                If MsgBox("Está acompanhado dos seus responsáveis legais? ", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                    resultado = permitido
+                ElseIf MsgBox("Está acompanhado dos seus responsáveis legais? ", MsgBoxStyle.YesNo) = MsgBoxResult.No Then
+                    MsgBox(" Acesso Negado" + vbCrLf + "Retorne novamamente acompanhado de seus responsáveis legais!", MsgBoxStyle.MsgBoxSetForeground, "Validação")
+                End If
+            End If
+
+        ElseIf idadeUsuario >= 12 Then
+            resultado = permitido
+        End If
+
+        txtResultado.Text = (resultado)
+
+
+
+
+
+    End Sub
+
+    Private Sub txtResultado_TextChanged(sender As Object, e As EventArgs) Handles txtResultado.TextChanged
+
+    End Sub
+End Class
